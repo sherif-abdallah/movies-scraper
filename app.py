51e7fc7ec1fc4e028b243d3f2589a870b3fa9c62
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2021-11-17 05:36:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import streamlit as st
+import pyperclip
 # Making a GET request
 
 
 
 hide_st_style = """
             <style>
-            #MainMenu {visibility: hidden;}
+
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
@@ -22,13 +22,18 @@ input = form.text_input("Enter Movie Url from shahed4u.land")
 submit =   form.form_submit_button("Submit")
 
 if submit:
+    try:
         r = requests.get(input)
         soup = BeautifulSoup(r.content, 'html.parser')
         vidhd = str(soup)
         vidhd_1 = vidhd.find('https://vidhd.fun')
-        vidhd = vidhd[vidhd_1:vidhd_1+45]
-        vidhd = 
+        vidhd = vidhd[vidhd_1:]
+        vidhd = vidhd[0: vidhd.index('"')]
+        pyperclip.copy(vidhd)
         st.code(vidhd)
 
+
+    except:
+        st.error('Cant Find Vidhd Link')
 
 st.caption('Made By [Flix](http://flix.pythonanywhere.com/)')

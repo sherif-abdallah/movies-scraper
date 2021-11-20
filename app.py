1f08f8201601563d09d5f56f254842d9581da50c
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2021-11-20 21:09:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import streamlit as st
-
-# Making a GET request
-
 
 
 hide_st_style = """
@@ -16,26 +13,69 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 form = st.form("my_form")
-input = form.text_input("Enter Movie Url from shahed4u.land")
-
-
+input = form.text_input("Enter Movie Url from mycima.ws")
 # Now add a submit button to the form:
 submit =   form.form_submit_button("Submit")
-
 if submit:
-    st.warning('Loading...')
     try:
-        r = requests.get(input)
-        soup = BeautifulSoup(r.content, 'html.parser')
-        vidhd = str(soup)
-        vidhd_1 = vidhd.find('https://vidhd.fun')
-        vidhd = vidhd[vidhd_1:]
-        vidhd = vidhd[0: vidhd.index('"')]
-
-        st.code(vidhd)
-
-
+        driver = webdriver.ChromeOptions()
+        driver.add_argument('headless')
+        driver = webdriver.Chrome(options=driver)
+        r = driver.get(input)
+        source = driver.page_source
+        soup = BeautifulSoup(source, "html.parser")
+        soup = str(soup)
+        # Get Links
     except:
-        st.error('Cant Find Vidhd Link')
+        st.error('Write a Valid Url')
 
-st.caption('Made By [Flix](http://flix.pythonanywhere.com/)')
+    try:
+        govid_1 = soup.find('https://govid.cyou')
+        govid = soup[govid_1:]
+        govid = govid[0: govid.index('?')]
+        govid = govid.strip(' ')
+    except:
+        govid = 'N/A'
+
+    try:
+        vidbom_1 = soup.find('https://vedbom.org')
+        vidbom = soup[vidbom_1:]
+        vidbom = vidbom[0: vidbom.index('?')]
+        vidbom = vidbom.strip(' ')
+    except:
+        vidbom = 'N/A'
+
+    try:
+        vedshare_1 = soup.find('https://vedshare.com')
+        vedshare = soup[vedshare_1:]
+        vedshare = vedshare[0: vedshare.index('?')]
+        vedshare = vedshare.strip(' ')
+    except:
+        vedshare = 'N/A'
+
+    try:
+        myviid_1 = soup.find('https://myviid.net')
+        myviid = soup[myviid_1:]
+        myviid = myviid[0: myviid.index('"')]
+        myviid = myviid.strip(' ')
+    except:
+        myviid = 'N/A'
+
+    try:
+        uqload_1 = soup.find('https://uqload.com')
+        uqload = soup[uqload_1:]
+        uqload = uqload[0: uqload.index('?')]
+        uqload = uqload.strip(' ')
+    except:
+        uqload = 'N/A'
+
+    try:
+        uptostream_1 = soup.find('https://uptostream.com')
+        uptostream = soup[uptostream_1:]
+        uptostream = uptostream[0: uptostream.index('?')]
+        uptostream = uptostream.strip(' ')
+    except:
+        uptostream = 'N/A'
+
+    # st.json({'Govid' : govid, 'Vidbom' : vidbom, 'Vedshare' : vedshare, 'myviid' : myviid, 'uqload':uqload, 'uptostream':uptostream})
+    st.table([['Govid' , govid], [ 'Vidbom' , vidbom], [ 'Vedshare' , vedshare], [ 'myviid' , myviid], [ 'uqload',uqload], [ 'uptostream',uptostream]])

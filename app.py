fc7228cdfd47ab68d4ea929f55d50e16b8045017
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2021-11-28 03:33:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
 import streamlit as st
+import re
+
+
 
 
 hide_st_style = """
@@ -8,74 +11,108 @@ hide_st_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
+            button[title="View fullscreen"] {
+            display: none;
+            }
+
+            button[title="View fullscreen"]:hover {
+            display: none;
+            }
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-form = st.form("my_form")
-input = form.text_input("Enter Movie Url from mycima.ws")
-# Now add a submit button to the form:
-submit =   form.form_submit_button("Submit")
-if submit:
-    driver = webdriver.ChromeOptions()
-    driver.add_argument('headless')
-    driver = webdriver.Chrome(options=driver)
-    r = driver.get(input)
-    source = driver.page_source
-    soup = BeautifulSoup(source, "html.parser")
-    soup = str(soup)
 
-            # Get Links
-#     except:
-#         st.error('Write a Valid Url')
 
-    try:
-        govid_1 = soup.find('https://govid.cyou')
-        govid = soup[govid_1:]
-        govid = govid[0: govid.index('?')]
-        govid = govid.strip(' ')
-    except:
-        govid = 'N/A'
+# form = st.form("my_form")
+input = st.text_input("Enter Movie Url from arabseed.ws")
+# submit = form.form_submit_button("Submit")
 
-    try:
-        vidbom_1 = soup.find('https://vedbom.org')
-        vidbom = soup[vidbom_1:]
-        vidbom = vidbom[0: vidbom.index('?')]
-        vidbom = vidbom.strip(' ')
-    except:
-        vidbom = 'N/A'
+try:
+    # INFO
+    r = requests.get(input)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    title = soup.find('h1', attrs = {'class':'Title'}) 
+    descrip = soup.find('p', attrs = {'class':'descrip'}) 
+    RatingImdb = soup.find('div', attrs = {'class':'RatingImdb'}) 
+    rate = RatingImdb.find('em')
+    image = soup.find('div', class_="PosterShape")
+    image = image.attrs.get('data-style')
+    image = re.search("(?P<url>https?://[^\s]+)", image).group("url")
+    image = str(image).replace(')', '').replace(';', '')
+    image = '''
+                <style>
+                .image{
+                    width: 200px;
+                    height: 350px;
+                }
+                @media screen and (min-width: 800px) {
+                    .image{
+                        width: 320px;
+                        height: 450px;
+                    }
+                }  
+                </style>              
+                <br><center><img class="image"  src="''' + image + '''" ></center><br>'''
+    st.markdown(image, unsafe_allow_html=True)
+    st.write('Title')
+    st.code(title.text)
+    st.write('Description')
+    st.code(descrip.text)
+    st.write('IMDB Rating')
+    st.code(rate.text)
+    MetaTermsInfo = soup.find('div', class_='MetaTermsInfo')
+    MetaTermsInfo = MetaTermsInfo.find_all('a')
+    st.table(MetaTermsInfo)
 
-    try:
-        vedshare_1 = soup.find('https://vedshare.com')
-        vedshare = soup[vedshare_1:]
-        vedshare = vedshare[0: vedshare.index('?')]
-        vedshare = vedshare.strip(' ')
-    except:
-        vedshare = 'N/A'
 
-    try:
-        myviid_1 = soup.find('https://myviid.net')
-        myviid = soup[myviid_1:]
-        myviid = myviid[0: myviid.index('"')]
-        myviid = myviid.strip(' ')
-    except:
-        myviid = 'N/A'
 
-    try:
-        uqload_1 = soup.find('https://uqload.com')
-        uqload = soup[uqload_1:]
-        uqload = uqload[0: uqload.index('?')]
-        uqload = uqload.strip(' ')
-    except:
-        uqload = 'N/A'
+    # Watch Servers
+    r = requests.get(input + '/watch/')
+    soup = BeautifulSoup(r.content, 'html5lib')
+    containerServers = soup.find('div', class_='containerServers')
+    allcontainerServers = containerServers.find_all('li')
+    option = st.selectbox('Choosse a Server',tuple( range(0, len(allcontainerServers))))
 
-    try:
-        uptostream_1 = soup.find('https://uptostream.com')
-        uptostream = soup[uptostream_1:]
-        uptostream = uptostream[0: uptostream.index('?')]
-        uptostream = uptostream.strip(' ')
-    except:
-        uptostream = 'N/A'
 
-    # st.json({'Govid' : govid, 'Vidbom' : vidbom, 'Vedshare' : vedshare, 'myviid' : myviid, 'uqload':uqload, 'uptostream':uptostream})
-    st.table([['Govid' , govid], [ 'Vidbom' , vidbom], [ 'Vedshare' , vedshare], [ 'myviid' , myviid], [ 'uqload',uqload], [ 'uptostream',uptostream]])
+    currentvideo = containerServers.find_all("li", {"data-server" : option})
+    currentvideo = str(currentvideo).replace('[', '').replace(']', '')
+    videosoup = BeautifulSoup(currentvideo, 'html5lib')
+    
+    videotitle = videosoup.find('span')
+    videotitle = str(videotitle.text).replace('p', '').replace('ٍ', '')
+    st.write('## ' + videotitle)
+
+    videosrc = videosoup.find('iframe')
+    st.code(videosrc['src'])
+
+    iframe = '''
+                <style>
+                .video{
+                    width: 350px;
+                    height: 250px;
+                }
+                @media screen and (min-width: 800px) {
+                    .video{
+                        width: 700px;
+                        height: 400px;
+                    }
+                }  
+                </style>              
+                <center><iframe class="video"  src="''' + videosrc['src'] + '''" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" sandbox="allow-scripts" sandboxscrolling="no" frameborder="0" ></iframe></center>'''
+    st.markdown(iframe, unsafe_allow_html=True)
+
+
+    # Download Servers
+    r = requests.get(input + '/download/')
+    soup = BeautifulSoup(r.content, 'html5lib')
+    DownloadArea = soup.find('div', class_='DownloadArea')
+    downloadserver = DownloadArea.find_all('a')
+    download_option = st.selectbox('Choosse a Download Server',tuple( range(0, len(downloadserver))))
+    st.write('Download ' + str(downloadserver[download_option].text).replace('p', '').strip())
+    st.code(downloadserver[download_option]['href'])
+
+
+except:
+    st.error('Please Write a valid URL')
+st.caption('Made By [Flix](http://flix.pythonanywhere.com/)')

2def921f9e7d024645c079a1bd7fb4e803283b26
Sherif Abdullah <85506496+sherif-abdallah@users.noreply.github.com>
2021-11-28 03:44:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -29,88 +29,88 @@ input = st.text_input("Enter Movie Url from arabseed.ws")
 # submit = form.form_submit_button("Submit")
 
 
-    # INFO
-    r = requests.get(input)
-    soup = BeautifulSoup(r.content )
-    title = soup.find('h1', attrs = {'class':'Title'}) 
-    descrip = soup.find('p', attrs = {'class':'descrip'}) 
-    RatingImdb = soup.find('div', attrs = {'class':'RatingImdb'}) 
-    rate = RatingImdb.find('em')
-    image = soup.find('div', class_="PosterShape")
-    image = image.attrs.get('data-style')
-    image = re.search("(?P<url>https?://[^\s]+)", image).group("url")
-    image = str(image).replace(')', '').replace(';', '')
-    image = '''
-                <style>
-                .image{
-                    width: 200px;
-                    height: 350px;
-                }
-                @media screen and (min-width: 800px) {
-                    .image{
-                        width: 320px;
-                        height: 450px;
-                    }
-                }  
-                </style>              
-                <br><center><img class="image"  src="''' + image + '''" ></center><br>'''
-    st.markdown(image, unsafe_allow_html=True)
-    st.write('Title')
-    st.code(title.text)
-    st.write('Description')
-    st.code(descrip.text)
-    st.write('IMDB Rating')
-    st.code(rate.text)
-    MetaTermsInfo = soup.find('div', class_='MetaTermsInfo')
-    MetaTermsInfo = MetaTermsInfo.find_all('a')
-    st.table(MetaTermsInfo)
+# INFO
+r = requests.get(input)
+soup = BeautifulSoup(r.content )
+title = soup.find('h1', attrs = {'class':'Title'}) 
+descrip = soup.find('p', attrs = {'class':'descrip'}) 
+RatingImdb = soup.find('div', attrs = {'class':'RatingImdb'}) 
+rate = RatingImdb.find('em')
+image = soup.find('div', class_="PosterShape")
+image = image.attrs.get('data-style')
+image = re.search("(?P<url>https?://[^\s]+)", image).group("url")
+image = str(image).replace(')', '').replace(';', '')
+image = '''
+    <style>
+    .image{
+        width: 200px;
+        height: 350px;
+    }
+    @media screen and (min-width: 800px) {
+        .image{
+            width: 320px;
+            height: 450px;
+        }
+    }  
+    </style>              
+    <br><center><img class="image"  src="''' + image + '''" ></center><br>'''
+st.markdown(image, unsafe_allow_html=True)
+st.write('Title')
+st.code(title.text)
+st.write('Description')
+st.code(descrip.text)
+st.write('IMDB Rating')
+st.code(rate.text)
+MetaTermsInfo = soup.find('div', class_='MetaTermsInfo')
+MetaTermsInfo = MetaTermsInfo.find_all('a')
+st.table(MetaTermsInfo)
 
 
 
-    # Watch Servers
-    r = requests.get(input + '/watch/')
-    soup = BeautifulSoup(r.content )
-    containerServers = soup.find('div', class_='containerServers')
-    allcontainerServers = containerServers.find_all('li')
-    option = st.selectbox('Choosse a Server',tuple( range(0, len(allcontainerServers))))
+# Watch Servers
+r = requests.get(input + '/watch/')
+soup = BeautifulSoup(r.content )
+containerServers = soup.find('div', class_='containerServers')
+allcontainerServers = containerServers.find_all('li')
+option = st.selectbox('Choosse a Server',tuple( range(0, len(allcontainerServers))))
 
 
-    currentvideo = containerServers.find_all("li", {"data-server" : option})
-    currentvideo = str(currentvideo).replace('[', '').replace(']', '')
-    videosoup = BeautifulSoup(currentvideo )
-    
-    videotitle = videosoup.find('span')
-    videotitle = str(videotitle.text).replace('p', '').replace('ٍ', '')
-    st.write('## ' + videotitle)
+currentvideo = containerServers.find_all("li", {"data-server" : option})
+currentvideo = str(currentvideo).replace('[', '').replace(']', '')
+videosoup = BeautifulSoup(currentvideo )
 
-    videosrc = videosoup.find('iframe')
-    st.code(videosrc['src'])
+videotitle = videosoup.find('span')
+videotitle = str(videotitle.text).replace('p', '').replace('ٍ', '')
+st.write('## ' + videotitle)
 
-    iframe = '''
-                <style>
-                .video{
-                    width: 350px;
-                    height: 250px;
-                }
-                @media screen and (min-width: 800px) {
-                    .video{
-                        width: 700px;
-                        height: 400px;
-                    }
-                }  
-                </style>              
-                <center><iframe class="video"  src="''' + videosrc['src'] + '''" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" sandbox="allow-scripts" sandboxscrolling="no" frameborder="0" ></iframe></center>'''
-    st.markdown(iframe, unsafe_allow_html=True)
+videosrc = videosoup.find('iframe')
+st.code(videosrc['src'])
+
+iframe = '''
+    <style>
+    .video{
+        width: 350px;
+        height: 250px;
+    }
+    @media screen and (min-width: 800px) {
+        .video{
+            width: 700px;
+            height: 400px;
+        }
+    }  
+    </style>              
+    <center><iframe class="video"  src="''' + videosrc['src'] + '''" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" sandbox="allow-scripts" sandboxscrolling="no" frameborder="0" ></iframe></center>'''
+st.markdown(iframe, unsafe_allow_html=True)
 
 
-    # Download Servers
-    r = requests.get(input + '/download/')
-    soup = BeautifulSoup(r.content )
-    DownloadArea = soup.find('div', class_='DownloadArea')
-    downloadserver = DownloadArea.find_all('a')
-    download_option = st.selectbox('Choosse a Download Server',tuple( range(0, len(downloadserver))))
-    st.write('Download ' + str(downloadserver[download_option].text).replace('p', '').strip())
-    st.code(downloadserver[download_option]['href'])
+# Download Servers
+r = requests.get(input + '/download/')
+soup = BeautifulSoup(r.content )
+DownloadArea = soup.find('div', class_='DownloadArea')
+downloadserver = DownloadArea.find_all('a')
+download_option = st.selectbox('Choosse a Download Server',tuple( range(0, len(downloadserver))))
+st.write('Download ' + str(downloadserver[download_option].text).replace('p', '').strip())
+st.code(downloadserver[download_option]['href'])
 
 
 st.caption('Made By [Flix](http://flix.pythonanywhere.com/)')
